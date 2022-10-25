18dc0dd5cf55f082c0f6e98640fcf4ba38d98ea5
Troy <wilson.troy.anthony@gmail.com>
2022-10-25 08:34:41 -0400

    Getting there

--- Typescript.js
+++ Typescript.js
@@ -1,2 +1,18 @@
 console.log("hello");
+var mountains = [
+    { name: "Kilimanjaro", height: 19341 },
+    { name: "Everest", height: 29029 },
+    { name: "Denali", height: 20310 }
+];
+function findNameOfTallestMountain(mountains) {
+    var tallest = mountains[0];
+    mountains.forEach(function (mountain) {
+        if (mountain.height > tallest.height) {
+            tallest = mountain;
+        }
+    });
+    return tallest.name;
+}
+var tallestMountain = findNameOfTallestMountain(mountains);
+console.log(tallestMountain);
 //# sourceMappingURL=Typescript.js.map
--- Typescript.ts
+++ Typescript.ts
@@ -1,1 +1,25 @@
-console.log("hello")
+console.log("hello");
+
+interface Mountain {
+    name: string;
+    height: number;
+}
+
+let mountains: Mountain[] = [
+    { name: "Kilimanjaro", height: 19341 },
+    { name: "Everest", height: 29029 },
+    { name: "Denali", height: 20310 }
+];
+
+function findNameOfTallestMountain(mountains: Mountain[]): string {
+    let tallest: Mountain = mountains[0];
+    mountains.forEach(mountain => {
+        if (mountain.height > tallest.height) {
+            tallest = mountain;
+        }
+    });
+    return tallest.name;
+}
+
+let tallestMountain = findNameOfTallestMountain(mountains);
+console.log(tallestMountain);
