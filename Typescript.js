14e0bb446064477c757297256b4c8588537a8fbd
Troy Wilson <102485470+Buggerdo@users.noreply.github.com>
2022-10-25 12:50:20 -0400

    That should be all

--- Typescript.js
+++ Typescript.js
@@ -1,4 +1,3 @@
-console.log("hello");
 var mountains = [
     { name: "Kilimanjaro", height: 19341 },
     { name: "Everest", height: 29029 },
@@ -15,4 +14,25 @@ function findNameOfTallestMountain(mountains) {
 }
 var tallestMountain = findNameOfTallestMountain(mountains);
 console.log(tallestMountain);
+var Products = [
+    { name: "Apple", price: 1.25 },
+    { name: "Banana", price: 0.25 },
+    { name: "Pear", price: 2.25 },
+    { name: "Grape", price: 3.25 },
+];
+function calcAverageProductPrice(product) {
+    return product.reduce(function (s, p) { return s + p.price; }, 0) / product.length;
+}
+var average = calcAverageProductPrice(Products);
+console.log(average);
+var inventory = [
+    { product: { name: "motor", price: 10.00 }, quantity: 10 },
+    { product: { name: "sensor", price: 12.50 }, quantity: 4 },
+    { product: { name: "LED", price: 1.00 }, quantity: 20 }
+];
+function calcInventoryValue(inventory) {
+    return inventory.reduce(function (s, i) { return s + i.product.price * i.quantity; }, 0);
+}
+var value = calcInventoryValue(inventory);
+console.log(value);
 //# sourceMappingURL=Typescript.js.map
--- Typescript.ts
+++ Typescript.ts
@@ -1,5 +1,3 @@
-console.log("hello");
-
 interface Mountain {
     name: string;
     height: number;
@@ -23,3 +21,42 @@ function findNameOfTallestMountain(mountains: Mountain[]): string {
 
 let tallestMountain = findNameOfTallestMountain(mountains);
 console.log(tallestMountain);
+ 
+
+interface Product {
+    name: string;
+    price: number;
+}
+
+let Products: Product[] = [
+    {name: "Apple", price: 1.25},
+    {name: "Banana", price: 0.25},
+    {name: "Pear", price: 2.25},
+    {name: "Grape", price: 3.25},
+];
+
+function calcAverageProductPrice(product: Product[]): number{
+    return product.reduce((s, p) => s + p.price, 0) / product.length;
+}
+
+let average = calcAverageProductPrice(Products);
+console.log(average);
+
+interface InventoryItem {
+    product: Product;
+    quantity: number;
+}
+
+let inventory: InventoryItem[] = [
+    {product: {name: "motor", price: 10.00}, quantity: 10},
+    {product: {name: "sensor", price: 12.50}, quantity: 4},
+    {product: {name: "LED", price: 1.00}, quantity: 20}
+];
+
+function calcInventoryValue(inventory: InventoryItem[]): number{
+    return inventory.reduce((s, i) => s + i.product.price * i.quantity, 0);
+}
+
+let value = calcInventoryValue(inventory);
+
+console.log(value);
